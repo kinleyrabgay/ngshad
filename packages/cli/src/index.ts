88d0ccbc73066a3b0c3f8ec5856c14dc942988b5
kinleyrabgay <fsd.rabgay@gmail.com>
2025-04-07 12:31:43 +0600

    package: fix
    Signed-off-by: kinleyrabgay <fsd.rabgay@gmail.com>

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,18 +1,14 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
-import { add } from './commands/add';
-import { init } from './commands/init';
+import add from './commands/add';
+import init from './commands/init';
 import { version } from '../package.json';
 
 const program = new Command();
 
 program.name('ngshad').description('CLI for managing NgShad UI components').version(version);
 
-program.command('init').description('Initialize NgShad in your project').action(init);
-
-program
-  .command('add <component>')
-  .description('Add a NgShad component to your project')
-  .action(add);
+program.addCommand(init);
+program.addCommand(add);
 
 program.parse();
